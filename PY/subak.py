4e529cd349603cfc04f7085bbea5a335c040363e
park jong ik <pji219@naver.com>
2020-11-09 23:27:40 +0900

    수박수박수? 파이썬 풀이

---
+++ PY/subak.py
@@ -0,0 +1,12 @@
+def sol(n):
+    answer = ''
+    
+    for i in range(1, n+1):
+        if i%2 == 0:
+            answer+= '박'
+        else:
+            answer+= '수'
+    
+    return answer
+
+print(sol(5))
