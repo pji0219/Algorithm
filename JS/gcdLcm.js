11745ac1aedd9caf206d47f07d9f9b52946a8d25
park jong ik <pji219@naver.com>
2021-04-13 22:46:42 +0900

    1

--- JS/gcdLcm.js
+++ JS/gcdLcm.js
@@ -3,28 +3,28 @@
 function soluction(num1, num2) {
     let answer = [];
 
-    function gcd (num1, num2) {
-    
+    function gcd(num1, num2) {
+
         let num = num1 > num2 ? num1 : num2;
         let maxNum;
-    
+
         for (let i = 1; i <= num; i++) {
             if (num1 % i === 0 && num2 % i === 0) {
                 maxNum = i;
             }
         }
-    
+
         return maxNum;
     }
-    
+
     function lcm(num1, num2) {
-        return num1 * num2 / gcd(num1,num2);
+        return num1 * num2 / gcd(num1, num2);
     }
 
-    answer.push(gcd(num1,num2));
-    answer.push(lcm(num1,num2));
+    answer.push(gcd(num1, num2));
+    answer.push(lcm(num1, num2));
 
     return answer;
 }
 
-console.log(soluction(3,12));
+console.log(soluction(1, 6));
