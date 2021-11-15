96bd00a2eb3998463571dd145a18f7620887b93a
pji0219 <pji219@naver.com>
2021-11-15 23:22:42 +0900

    Math 내장함수로 최소값 최대값 구하기

---
+++ JS/section1/6.js
@@ -0,0 +1,21 @@
+// Math 내장함수로  최소값, 최대값 구하기
+
+// 최소값
+function minSolution(arr) {
+  let answer = Math.min(...arr);
+
+  return answer;
+}
+
+const arr1 = [5, 7, 1, 3, 2, 9, 11];
+console.log(minSolution(arr1));
+
+// 최대값
+function maxSolution(arr) {
+  let answer = Math.max(...arr);
+
+  return answer;
+}
+
+const arr2 = [5, 7, 1, 3, 2, 9, 11];
+console.log(maxSolution(arr2));
