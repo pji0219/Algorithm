2ef6916590fa6ec403d5dc4a60869b6f1fc10923
pji0219 <pji219@naver.com>
2021-11-26 11:44:53 +0900

    홀수

--- JS/section1/7.js
+++ JS/section1/7.js
@@ -1,12 +1,47 @@
-function solution(a, b, c) {
-  let answer;
+// 7개의 수중 홀수들을 골라 합을 구하고 고른 홀수들중 최소값을 구하라.
 
-  if (a < b) answer = a;
-  else answer = b;
+// me
+function solution(arr) {
+  let answer = [];
+  let oddNum;
+  let min = 100;
 
-  if (c < answer) answer = c;
+  // 홀수 구하고
+  oddNum = arr.filter((num) => num % 2 !== 0);
+
+  // 구한 홀수를 합함
+  const sum = oddNum.reduce((acc, crr) => acc + crr, 0);
+  answer.push(sum);
+
+  // 구한 홀수들중 최소값을 구함
+  arr
+    .filter((num) => num % 2 != 0)
+    .forEach((num) => {
+      if (num < min) min = num;
+    });
+  answer.push(min);
 
   return answer;
 }
 
-console.log(solution(3, 2, 1));
+arr = [12, 77, 38, 41, 53, 92, 85];
+console.log(solution(arr));
+
+// 2
+function solution2(arr) {
+  let answer = [];
+  let sum = 0,
+    min = 100;
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      sum += x;
+      if (x < min) min = x;
+    }
+  }
+  answer.push(sum);
+  answer.push(min);
+  return answer;
+}
+
+arr2 = [12, 77, 38, 41, 53, 92, 85];
+console.log(solution2(arr2));
