fbcc0d9eaf16f310f436e96b1a0faf36680a722a
pji0219 <pji219@naver.com>
2022-05-28 23:45:19 +0900

    문제 풀이

--- JS/section1/5.js
+++ JS/section1/5.js
@@ -6,6 +6,12 @@ function solution(arr) {
   // 일단 안정적인 큰 수를 할당해 놓음, 배열의 처음 수로 어떤 수가 파라미터로 오든지 비교해서 할당 시키기 위해
   let min = Number.MAX_SAFE_INTEGER;
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+  }
+
+  answer = min;
+
   return answer;
 }
 
