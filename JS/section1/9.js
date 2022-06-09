4a8f12b0b74b2b0e7fe05ad0bbcc7469657a0862
pji0219 <pji219@naver.com>
2022-06-09 23:55:22 +0900

    1

--- JS/section1/9.js
+++ JS/section1/9.js
@@ -1,15 +1,12 @@
-// 일곱난쟁이
+// 일곱난쟁이, 일곱난쟁이 키의 합이 100이라는 단서로 9명중 진짜 일곱난쟁이를 구하라
 function solution(arr) {
   let answer = arr;
-  let sum = answer.reduce((a, b) => a + b, 0);
+  let sum = answer.reduce((acc, crr) => acc + crr, 0);
+
   for (let i = 0; i < 8; i++) {
-    for (let j = i + 1; j < 9; j++) {
-      if (sum - (answer[i] + answer[j]) == 100) {
-        answer.splice(j, 1);
-        answer.splice(i, 1);
-      }
-    }
+    for (let j = 1; j < 9; j++) {}
   }
+
   return answer;
 }
 
