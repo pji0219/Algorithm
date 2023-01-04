ee2dc70645719f3cf0f90dcf5493256d9777649f
pji0219 <pji219@naver.com>
2023-01-04 23:47:44 +0900

    대소문자 변환

---
+++ JS/section1/14.js
@@ -0,0 +1,10 @@
+function solution(str) {
+  let answer = '';
+  for (let s of str) {
+    if (s === s.toUpperCase()) answer += s.toLowerCase();
+    else answer += s.toUpperCase();
+  }
+  return answer;
+}
+
+console.log(solution('StuDY'));
