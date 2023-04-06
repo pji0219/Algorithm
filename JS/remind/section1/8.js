e7e9f8d192c7e6b8f044d9f8e13ee038e4020fb8
pji0219 <pji219@naver.com>
2023-04-06 23:41:35 +0900

    문제 풀이

--- JS/remind/section1/8.js
+++ JS/remind/section1/8.js
@@ -1,8 +1,8 @@
 // 10부제, 날짜의 일의 자리와 차량 번호의 일의 자리가 일치시 운행 금지
 function solution(day, arr) {
   let answer = 0;
+
   for (i of arr) {
-    // 어떤 수를 10으로 나누었을 때 나머지는 일의 자리가 나옴 그것으로 날짜와 같은지 판단
     if (i % 10 === day) answer++;
   }
 
