719269863411559817a85f54b066d5e89533dbdc
pji0219 <pji219@naver.com>
2023-04-23 23:39:03 +0900

    문풀

---
+++ JS/remind/section1/17.js
@@ -0,0 +1,11 @@
+// 중복 문자 제거
+function solution(str) {
+  let answer = '';
+  //console.log(str.indexOf("K"));
+  for (let i = 0; i < str.length; i++) {
+    //console.log(str[i], i, str.indexOf(str[i]));
+    if (str.indexOf(str[i]) === i) answer += str[i];
+  }
+  return answer;
+}
+console.log(solution('ksekkset'));
