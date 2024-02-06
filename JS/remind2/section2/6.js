f5cfc975c8d407b4b4ed30964124ab2eb4d82fbd
pji0219 <pji219@naver.com>
2024-02-07 00:44:20 +0900

    격자판 최대합

--- JS/remind2/section2/6.js
+++ JS/remind2/section2/6.js
@@ -1,11 +1,12 @@
 // 격자판 최대합
 function solution(arr) {
   let answer = Number.MIN_SAFE_INTEGER;
-  let sum1,
-    sum2 = 0;
+  let sum1 = 0;
+  let sum2 = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    sum1, (sum2 = 0);
+    sum1 = 0;
+    sum2 = 0;
     for (let j = 0; j < arr.length; j++) {
       sum1 += arr[i][j];
       sum2 += arr[j][i];
@@ -13,5 +14,24 @@ function solution(arr) {
     answer = Math.max(answer, sum1, sum2);
   }
 
-  sum1, (sum2 = 0);
+  sum1 = 0;
+  sum2 = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum1 += arr[i][i];
+    sum2 += arr[i][arr.length - i - 1];
+  }
+  answer = Math.max(answer, sum1, sum2);
+
+  return answer;
 }
+
+arr = [
+  [10, 13, 10, 12, 15],
+  [12, 39, 30, 23, 11],
+  [11, 25, 50, 53, 15],
+  [19, 27, 29, 37, 27],
+  [19, 13, 30, 13, 19],
+];
+
+console.log(solution(arr));
